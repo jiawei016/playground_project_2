073ab9aa04758598dcdd6c610bfb0a5fced33391
jiawei016 <jeremy_wei92@outlook.com>
2023-09-09 12:11:59 +0800

    added mongo db

--- CarSellingPlatform/src/AuctionService/Program.cs
+++ CarSellingPlatform/src/AuctionService/Program.cs
@@ -7,13 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AuctionDbContext>(options => {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetSection("PostgresConfiguration").GetSection("ConnectionString").Value);
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
